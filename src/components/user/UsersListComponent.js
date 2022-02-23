92714211e5656c0c54419074aba07cfe5834919a
TSVlad <vladislav.kolmykov@yandex.ru>
2022-02-23 16:59:57 +0300

    fix uninitialized request

--- src/components/user/UsersListComponent.js
+++ src/components/user/UsersListComponent.js
@@ -1,26 +1,32 @@
 import UserItemComponent from "./UserItemComponent";
 import {useEffect, useState} from "react";
 import {getSubscriptionsRequest} from "../../utills/request/requests/notificationRequests";
+import {useKeycloak} from "@react-keycloak/web";
 
 const UsersListComponent = (props) => {
     const [subscriptions, setSubscriptions] = useState([])
+
+    const {initialized} = useKeycloak()
+
     useEffect(() => {
-        if (!props.subscriptions) {
-            getSubscriptionsRequest()
-                .then(response => {
-                    if (response.status === 200) {
-                        return response.json()
-                    } else {
-                        throw response
-                    }
-                })
-                .then(subs => {
-                    setSubscriptions(subs)
-                })
-        } else {
-            setSubscriptions(props.subscriptions)
+        if (initialized) {
+            if (!props.subscriptions) {
+                getSubscriptionsRequest()
+                    .then(response => {
+                        if (response.status === 200) {
+                            return response.json()
+                        } else {
+                            throw response
+                        }
+                    })
+                    .then(subs => {
+                        setSubscriptions(subs)
+                    })
+            } else {
+                setSubscriptions(props.subscriptions)
+            }
         }
-    }, [props])
+    }, [props, initialized])
 
     return (
         <div className={props.className} style={props.style}>
